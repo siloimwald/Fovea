ac40e7960c177e444f24a0b441e515a3242cb9fd
Mathias <siloimwald@gmail.com>
2023-09-07 20:43:08 +0200

    updated readme, fixed ignored teapot.obj

--- Fovea.CmdLine/Program.cs
+++ Fovea.CmdLine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CommandLine;
 using Fovea.Renderer.Core;
 
 namespace Fovea.CmdLine
@@ -7,31 +8,30 @@ namespace Fovea.CmdLine
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("rendering all test scenes...");
-            foreach (int i in Enum.GetValues(typeof(DemoScenes)))
-            {
-                var fileName = $"{Enum.GetName(typeof(DemoScenes), i)}.ppm";
-                Console.WriteLine($"rendering {fileName}");
-                var renderer = new Raytracer
-                {
-                    NumSamples = 10
-                };
-                var scene = DemoSceneCreator.MakeScene((DemoScenes)i, 512);
-                renderer.Render(scene, fileName);
-            }
-            
-            // Parser.Default.ParseArguments<CommandLineArgs>(args)
-            //     .WithParsed(opts =>
+            // Console.WriteLine("rendering all test scenes...");
+            // foreach (int i in Enum.GetValues(typeof(DemoScenes)))
+            // {
+            //     var fileName = $"{Enum.GetName(typeof(DemoScenes), i)}.ppm";
+            //     Console.WriteLine($"rendering {fileName}");
+            //     var renderer = new Raytracer
             //     {
-            //         var renderer = new Raytracer
-            //         {
-            //             NumSamples = opts.NumSamples
-            //         };
-            //         var foo = DemoScenes.GetValuesAsUnderlyingType<int>()
-            //         
-            //         var scene = DemoSceneCreator.MakeScene(DemoScenes.FinalSceneBookTwo, opts.ImageWidth);
-            //         renderer.Render(scene);
-            //     });
+            //         NumSamples = 10
+            //     };
+            //     var scene = DemoSceneCreator.MakeScene((DemoScenes)i, 512);
+            //     renderer.Render(scene, fileName);
+            // }
+            
+            Parser.Default.ParseArguments<CommandLineArgs>(args)
+                .WithParsed(opts =>
+                {
+                    var renderer = new Raytracer
+                    {
+                        NumSamples = opts.NumSamples
+                    };
+                    
+                    var scene = DemoSceneCreator.MakeScene(DemoScenes.FinalSceneBookTwo, opts.ImageWidth);
+                    renderer.Render(scene);
+                });
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 ### Ray Tracing in One Weekend (and more)
 
+Update: currently in the process of adapting this to version 4 of the books,
+some things are broken (notably, depth of field). Replaced custom Vec3/Point3 implementations with
+the `System.Numerics` Vector3 variants, leads to some artifacts.
+
 C# implementation of Peter Shirley's excellent free books on ray and path tracing
 with some own additions. [Take me to the books!](https://raytracing.github.io/)
 
@@ -8,9 +12,6 @@ Fovea is the name i usually pick for all of my hobbyist rendering adventures, fr
 
 In addition to the books
 
-- basic CSG support based on
-["Ray Tracing CSG Objects Using Single Hit Intersections"](http://xrt.wdfiles.com/local--files/doc%3Acsg/CSG.pdf)
-  (though slightly broken when using instancing, can't be bothered to fix that ;)
 - more general instancing support through transformation matrices
 - cylinders and disks, because why not
 - slightly more fancy BVH (iirc based on pbrt), using a SAH approach with binning
@@ -19,7 +20,7 @@ In addition to the books
 quads/boxes from triangles. i.e. the cornell box is actually all triangles except for the sphere.
 
 Compile and run:
-Should probably compile and run fine with any recent dotnet core or the normal .NET Framework. I've used .NET 5.
+Should probably compile and run fine with any recent dotnet core. (currently NET7)
 The command line executable accepts two parameters:
 - -s samples
 - -w image width (the height is derived from the aspect ratio)
